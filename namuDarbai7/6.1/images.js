592bbf39a5833cb4991f9ded37a303c7dc6a2698
mantask1 <mantas.kazonas@gmail.com>
2021-03-10 15:42:20 +0200

    homework6

---
+++ namuDarbai7/6.1/images.js
@@ -0,0 +1,12 @@
+function getImages(){
+    var images = ["https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/bycycle.jpg&quot",
+                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/chess.jpg&quot",
+                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/jump.jpg&quot", "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/kiting.jpg&quot", "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/lion.jpg&quot",
+                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/onwater.jpg&quot",
+                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/para.jpg&quot",
+                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/photographer.jpg&quot",
+                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/pieva.jpg&quot",
+                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/supynes.jpg&quot"];
+
+    return images;
+}
