7a64a0c58d859b2971a1133ada0b9dbe9ae3de9e
mantask1 <mantas.kazonas@gmail.com>
2021-03-12 14:39:00 +0200

    homework6

---
+++ namuDarbai7/6.2/service.js
@@ -0,0 +1,48 @@
+function getComputers(){
+    return [
+        {
+            "id": 1,
+            "item": "Nešiojamas kompiuteris ASUS K540NA N3450/4/500GB/Win",
+            "image": "https://www.topocentras.lt/cdn-cgi/image/fit=contain,format=auto,quality=80/media/catalog/product/1/1/111_330378_1600666894.jpg",
+            "descriptions": [
+                "Atmintinė (RAM): 4(GB)",
+                "Įstrižainė: 15.6\" (39,6cm)"
+            ],
+            "price": 259.00,
+            "oldprice": 359.00
+        },
+        {
+            "id": 2,
+            "item": "Nešiojamas kompiuteris LENOVO V110-15IAP N3350/4/500GB/DOS",
+            "image": "https://www.topocentras.lt/cdn-cgi/image/fit=contain,format=auto,quality=80/media/catalog/product/1/1/111_256433_1565606621.jpg",
+            "descriptions": [
+                "Atmintinė (RAM): 4(GB)",
+                "Įstrižainė: 15.6\" (~39,6cm)"
+            ],
+            "price": 199.99,
+            "oldprice": 349.99
+        },
+        {
+            "id": 3,
+            "item": "Nešiojamas kompiuteris DELL Inspiron 15 3573 N5000/4/500GB/Win",
+            "image": "https://www.topocentras.lt/cdn-cgi/image/fit=contain,format=auto,quality=80/media/catalog/product/1/1/111_350517_1606805087.jpg",
+            "descriptions": [
+                "Atmintinė (RAM): 4(GB)",
+                "Įstrižainė: 15.6\" (39,6cm)"
+            ],
+            "price": 299.00,
+            "oldprice": 399.00
+        },
+        {
+            "id": 4,
+            "item": "Nešiojamas kompiuteris DELL Inspiron 15 3573 N5000/4/500GB/Win",
+            "image": "https://www.topocentras.lt/cdn-cgi/image/fit=contain,format=auto,quality=80/media/catalog/product/t/m/tmp5c19_341075_1603442793.jpg",
+            "descriptions": [
+                "Atmintinė (RAM): 4(GB)",
+                "Įstrižainė: 15.6\" (39,6cm)"
+            ],
+            "price": 299.00,
+            "oldprice": 399.00
+        }
+    ];
+}
