beffe1bfbaf52d55bff8d7bc9df163fbdbf9fa8a
mantask1 <mantas.kazonas@gmail.com>
2021-03-12 11:46:56 +0200

    homework6 fixed

--- namuDarbai7/6.1/images.js
+++ namuDarbai7/6.1/images.js
@@ -1,12 +1,11 @@
 function getImages(){
-    var images = ["https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/bycycle.jpg&quot",
-                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/chess.jpg&quot",
-                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/jump.jpg&quot", "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/kiting.jpg&quot", "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/lion.jpg&quot",
-                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/onwater.jpg&quot",
-                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/para.jpg&quot",
-                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/photographer.jpg&quot",
-                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/pieva.jpg&quot",
-                  "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/supynes.jpg&quot"];
-
-    return images;
+    return ["https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/bycycle.jpg",
+            "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/chess.jpg",
+            "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/jump.jpg", "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/kiting.jpg", "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/lion.jpg",
+            "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/onwater.jpg",
+            "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/para.jpg",
+            "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/photographer.jpg",
+            "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/pieva.jpg",
+            "https://raw.githubusercontent.com/zigmantasvcs/resources/master/images/offers/supynes.jpg"
+           ];
 }
