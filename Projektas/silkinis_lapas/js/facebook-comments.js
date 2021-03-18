86b59d5fa87b87446d0b5955c327e458870b2715
mantask1 <mantas.kazonas@gmail.com>
2021-03-18 11:34:21 +0200

    project added3

---
+++ Projektas/silkinis_lapas/js/facebook-comments.js
@@ -0,0 +1,40 @@
+function getComments(){
+   return [
+        {
+            "createdTime": "2021-01-17",
+            "message": "Norim padėkoti už labai gražią palinkėjimų knygą ir tą extrą paveiksliuką. Su Jumis bendravimas buvo labai lengvas, viską išaiškinote ir likome sužavėti Jūsų draugiškumu. Ačiū labai ir manau musu keliai dar susitiks ❤️",
+            "id": "{page-id}_2191966997525824"
+        },
+
+        {
+            "createdTime": "2021-01-18",
+            "message": "Dėkojame už nuostabius pakvietimus ir palinkėjimų knygą 😊❤️ laukiam nesulaukiam karantino pabaigos, kada galėsime juos įteikti svečiams 😍❣️",
+            "id": "{page-id}_2191966997525824"
+        },
+
+        {
+            "createdTime": "2021-02-17",
+            "message": "Labai džiaugiuosi pasirinkusi Šilkinį lapá svečiú knygos gamybai! 😇 Planavimo procesas buvo greitas, bet tuo pačiu ir smulkmeniškas, aptarėm kiekviená detalé, todėl buvau įsitikinus, kad bus gerai. O gavau dar daugiau nei tikėjaus - gražus pateikimas, kruopštus ir kokybiškas darbas😍",
+            "id": "{page-id}_2191966997525824"
+        },
+
+        {
+            "createdTime": "2021-03-17",
+            "message": "Palinkėjimų knyga tobula. 😍 Malonus bendravimas, aptarėm kiekvieną smulkmeną, išpildė visus norus. Rekomenduoju 💯💯 🌸",
+            "id": "{page-id}_2191966997525824"
+        }
+
+    ];
+}
+
+
+
+//    "paging": {
+//    "cursors": {
+//      "before": "Q2c4U1pXNT...",
+//      "after": "Q2c4U1pXNT..."
+//    },
+//    "next": "https://graph.facebook.com/vX.X/{page-id}/feed?access_token={your-page-access-token}&pretty=0&limit=25&after=Q2c4U1pXNT..."
+//  }
+//
+//}
