768f0ba853a6e1ad2e6ff0727b5de8a1b8697355
mantask1 <mantas.kazonas@gmail.com>
2021-03-27 08:10:38 +0200

    JS exercises fixed

--- Papildomos_JS_uzduotys/JS5/loop.js
+++ Papildomos_JS_uzduotys/JS5/loop.js
@@ -1,13 +1,8 @@
 //<!--Apšilimas su ciklais. Parašykite funkciją, kuriai padavus skaičių X, funkcija išspausdintų jūsų vardą x kartų. Galite rinktis kokį ciklą for ar while naudosite.-->
 
 function printName(numberX){
-//    document.getElementsByTagName("body");
-    var numberX = 0;
-    var  text = "Mantas";
     for(var i=0;i < numberX;i++){
-        console.log(text);
-//        var element = document.createElement("div");
-//        element.innerHTML = numberX;
-//        body.appendChild(element);
+        var  text = "Mantas";
+        alert(text);
     }
 }
