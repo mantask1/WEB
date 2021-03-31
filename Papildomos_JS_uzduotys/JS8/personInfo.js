f618a3cae7945aa345e30b26397f3a3b80caf873
mantask1 <mantas.kazonas@gmail.com>
2021-03-31 21:33:45 +0300

    JS_exercise_added

--- Papildomos_JS_uzduotys/JS8/personInfo.js
+++ Papildomos_JS_uzduotys/JS8/personInfo.js
@@ -1,5 +1,20 @@
-function printListOfPerson(list[]) {
-    let list = ["Jonas Jonaitis", "Onutė Pakalnutė", "Juozas Juozaitis", "Marytė Rytė", "Antanas Antanaitis"];
+//Duotas vardų ir pavardžių masyvas:
+//let sarasas = ["Jonas Jonaitis", "Onutė Pakalnutė", "Juozas Juozaitis", "Marytė Rytė", "Antanas Antanaitis"];
+////Jums reikia parašyti funkciją, kuriai padavus šitą masyvą kaip argumentą, ši jį išspausdintų į sąrašą taip kaip pateikta paveikslėlyje:-->
 
 
+function printListOfPersons() {
+    let list = [
+        "Jonas Jonaitis",
+        "Onutė Pakalnutė",
+        "Juozas Juozaitis",
+        "Marytė Rytė",
+        "Antanas Antanaitis"
+    ];
+
+    let liElements = document.getElementsByTagName("li");
+
+    for(let i = 0; i < list.length; i++) {
+        liElements[i].innerHTML = list[i];
+    }
 }
