aab1a9453dac889915f4125083081ad52ae78b7a
mantask1 <mantas.kazonas@gmail.com>
2021-04-11 10:52:18 +0300

    JS4_fixed

--- Papildomos_JS_uzduotys/JS4/text.js
+++ Papildomos_JS_uzduotys/JS4/text.js
@@ -1,5 +1,5 @@
 //<!--Parašykite funkciją, kuriai padavus argumentus name ir surname funkcija išspausdintų konsolėje pranešimą. Pvz. jeigu name yra Jonas, o surname yra Jonaitis, tai funkcija turi išspausdinti "Jonas Jonaitis kątik prisijungė".-->
 
 function printInfoAboutPerson(name, surname) {
-    console.log(name + surname + " " + "ką tik prisijungė!");
+    console.log(name + " " + surname + " " + "ką tik prisijungė!");
 }
