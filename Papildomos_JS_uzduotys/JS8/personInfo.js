b3fc773b3e70ba8317458d9d8d4f6c32abe2b804
mantask1 <mantas.kazonas@gmail.com>
2021-04-11 11:07:19 +0300

    JS8_fixed

--- Papildomos_JS_uzduotys/JS8/personInfo.js
+++ Papildomos_JS_uzduotys/JS8/personInfo.js
@@ -2,9 +2,8 @@
 //let sarasas = ["Jonas Jonaitis", "Onutė Pakalnutė", "Juozas Juozaitis", "Marytė Rytė", "Antanas Antanaitis"];
 ////Jums reikia parašyti funkciją, kuriai padavus šitą masyvą kaip argumentą, ši jį išspausdintų į sąrašą taip kaip pateikta paveikslėlyje:-->
 
-
 function printListOfPersons() {
-    let list = [
+    let listArray = [
         "Jonas Jonaitis",
         "Onutė Pakalnutė",
         "Juozas Juozaitis",
@@ -12,9 +11,16 @@ function printListOfPersons() {
         "Antanas Antanaitis"
     ];
 
-    let liElements = document.getElementsByTagName("li");
+    let olElements = document.getElementsByTagName("ol")[0];
 
-    for(let i = 0; i < list.length; i++) {
-        liElements[i].innerHTML = list[i];
+    for (i = 0; i < listArray.length; i++) {
+
+        let element = document.createElement("li");
+
+        element.innerHTML = listArray[i];
+
+        olElements.appendChild(element);
     }
 }
+
+
