cfb5a21047ff870e0d32a46bd2970673b6f8c390
mantask1 <mantas.kazonas@gmail.com>
2021-04-11 11:14:42 +0300

    JS7_fixed

--- Papildomos_JS_uzduotys/JS7/mylibrary.js
+++ Papildomos_JS_uzduotys/JS7/mylibrary.js
@@ -11,15 +11,15 @@ function printDayOfWeekByNumber(index) {
 
     if (isNaN(index)) {
         let text = "Klaida. Įveskite skaičių nuo 1 iki 7.";
-        $(".container").append("<div>"+text+"</div>")
+        $(".container").append("<div>" + text + "</div>")
     }
     else if (index < 1 || index > 7) {
         let text = "Klaida. Įveskite skaičių nuo 1 iki 7.";
-        $(".container").append("<div>"+text+"</div>")
+        $(".container").append("<div>" + text + "</div>")
     }
     else {
         let text = weekDays[index - 1];
-        $(".container").append("<div>"+text+"</div>")
+        $(".container").append("<div>" + text + "</div>")
 
         //        let divElements = document.getElementsByTagName("div");
         //
